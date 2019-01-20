a2b3c5836541d80148e752db319c67dab2927866
Malaika Vijay <31766830+mvijay97@users.noreply.github.com>
2019-01-20 12:25:08 +0530

    Add files via upload

---
+++ CC/cc.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+from werkzeug.utils import secure_filename
+import os 
+
+app= Flask(__name__)
+
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET','POST'])
+def upload():
+	if(request.method=='POST'):
+		file = request.files['upload']
+		filename = secure_filename(file.filename)
+		src = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+		print(src)
+		file.save(src)
+		return(render_template("upload.html", filename="../"+src))
+	return(render_template("upload.html"))
+
+
+@app.route('/feed')
+def display_grid():
+	images=os.listdir('static')
+	images=["../"+os.path.join(app.config['UPLOAD_FOLDER'], file) for file in images]
+	return(render_template("feed.html",images=images))
+
