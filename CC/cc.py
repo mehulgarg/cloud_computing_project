33b0aa8eae2b0deeeaa5b25bca82569a95428c7b
Ninaad R. Rao <35262832+NinaadRao@users.noreply.github.com>
2019-01-22 06:40:41 +0530

    reading from database
    displaying in ascending order

--- CC/cc.py
+++ CC/cc.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,request
 from werkzeug.utils import secure_filename
 import os 
 import pandas as pd
+import datetime
 
 app= Flask(__name__)
 
@@ -14,21 +15,41 @@ def upload():
 		file = request.files['upload']
 		username=request.form['username']
 		tags=request.form['tags']
+		tags=tags.split(',')
+		tags=';'.join(tags)
 		caption=request.form['caption']
 		print(username,tags,caption)
 		filename = secure_filename(file.filename)
 		src = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 		print(src)
+		time1=datetime.datetime.now()
 		file.save(src)
+		likes=0
 		df=pd.read_csv('database.csv')
-		
+		print(type(df['user_name']))
+
+		print(df.columns)
+		#print(len(df.rows))
+		print(type(df['time'][0]),)
+		content=[username,src,likes,caption,tags,time1]
+		#df2=pd.DataFrame(content,columns=df.columns)
+		dictionary=dict(zip(df.columns,content))
+		print(dictionary)
+		df=df.append(dictionary,ignore_index=True)
+		df['time'] = df['time'].astype('datetime64[ns]')
+
+		#print(df['time'],type(df['time']))
+		df.sort_values(by=['time'],axis=0,inplace=True,ascending=[False])
+		df.to_csv('database.csv',index=False)
 		return(render_template("upload.html", filename="../"+src))
 	return(render_template("upload.html"))
 
 
 @app.route('/feed')
 def display_grid():
-	images=os.listdir('static')
+	#images=os.listdir('static')
+	df=pd.read_csv('database.csv')
+	images=[i.split('static/')[1] for i in df['image_src']]
 	images=["../"+os.path.join(app.config['UPLOAD_FOLDER'], file) for file in images]
 	return(render_template("feed.html",images=images))
 
