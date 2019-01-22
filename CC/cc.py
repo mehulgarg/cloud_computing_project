89a52e35a6ef28849cf95455c6f5fe23ad8d7337
Mehul Garg <mehulgarg14@gmail.com>
2019-01-22 10:28:13 +0530

    Fixed Form and made images look like cards

--- CC/cc.py
+++ CC/cc.py
@@ -15,13 +15,15 @@ def upload():
 		src = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 		print(src)
 		file.save(src)
-		return(render_template("upload.html", filename="../"+src))
-	return(render_template("upload.html"))
+		return render_template("upload.html", filename="../"+src)
+	return	render_template("upload.html")
 
 
 @app.route('/feed')
 def display_grid():
 	images=os.listdir('static')
 	images=["../"+os.path.join(app.config['UPLOAD_FOLDER'], file) for file in images]
-	return(render_template("feed.html",images=images))
+	return render_template("feed.html",images=images)
 
+if __name__ == '__main__':
+	app.run(debug = True)
