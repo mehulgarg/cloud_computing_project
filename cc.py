958f9f78edbd458f5d8fc88a21df307992257398
Mehul Garg <mehulgarg14@gmail.com>
2019-01-24 21:24:19 +0530

    Fixed image path in search function

--- cc.py
+++ cc.py
@@ -24,7 +24,8 @@ def searching():
 		for j in tags:
 			l_tags.append(j)
 			if(search==j.strip()):
-				l.append(df['image_src'][i])
+				img_path = df['image_src'][i].split('static/')[1]
+				l.append(img_path)
 				l.append(df['caption'][i])
 				l.append(l_tags)
 				images.append(l)
