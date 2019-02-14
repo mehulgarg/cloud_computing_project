82baf538b258d0e732858ceeb3cc1d21adbd59e0
mvijay97 <malaika.vijay@gmail.com>
2019-02-15 00:10:49 +0530

    updated api

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,jsonify,make_response,Response
 from flask_restful import Resource, Api
-from pymongo import MongoClient
+from pymongo import MongoClient,ReturnDocument
 import re
 import base64
 from datetime import datetime
@@ -9,13 +9,31 @@ import os
 import copy
 
 pwd_validate = re.compile(r'\b[0-9a-f]{40}\b')
-timestamp_validate = re.compile(r'\b[0-2][0-9]-(0[1-9]|1[0-2])-[0-9][0-9][0-9][0-9]:[0-6][0-9]-[0-5][0-9]-[0-2][0-4]\b')
+timestamp_validate = re.compile(r'\b[0-2][0-9]-(0[1-9]|1[0-2])-[0-9][0-9][0-9][0-9]:[0-6][0-9]-[0-5][0-9]-[0-2][0-9]\b')
 
 
 app = Flask(__name__)
 api = Api(app)
 
+class Authenticate(Resource):
+	def post(self):
+		request_json = request.get_json()
+		print(request_json)
+		username = request_json['username']
+		password = request_json['password']
 
+		client = MongoClient("mongodb://localhost:27017/")
+		db = client.sla
+		users = db.users
+
+		response=Response()
+		user_doc=users.find_one({"username":username,"password":password})
+		print(user_doc)
+		if(user_doc!=None):
+			response.status_code=200
+		else:
+			response.status_code=403
+		return(response)
 
 class AddUser(Resource):
 	def post(self):
@@ -98,12 +116,18 @@ class AddCategory(Resource):
 		valid_cat = col.find_one({"category":cat})
 		if(valid_cat==None):
 			cat_id=col.insert_one({"category" : cat, "count" : 0})
-			return(Response(status=201)) #Created
+			response=Response()
+			response.status_code=201
+			return(response)
 
-		return(Response(status=400)) #Bad request
+		response=Response()
+		response.status_code=400
+		return(response)
 
 	def delete(self):
-		return(Response(405))
+		response=Response()
+		response.status_code=405
+		return(response)
 
 	def get(self):
 		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
@@ -121,27 +145,37 @@ class AddCategory(Resource):
 			response = jsonify(dict(zip(keys,vals)))
 			response.status_code=200
 			return(response) #Found and returned
-		return(Response(204)) #No categories found
+
+		response=Response()
+		response.status_code=204
+		return(response)
 		
-
-
-
 class DelCategory(Resource):
 	def delete(self, category):
 		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+		print(client)
 		db = client.sla
 		col = db.categories
+		response=Response()
 
 		if(col.find_one({"category" : category})!=None):
 			cat_id = col.delete_one({"category" : category})
-			return(Response(200))
+			response.status_code=200
+			return(response)
 
-		return(Response(400))
+		response.status_code=400
+		return(response)
 
 	def get(self,category):
-		return(Response(405))
+		response=Response()
+		response.status_code=405
+		return(response)
+
+
 	def post(self,category):
-		return(Response(405))
+		response=Response()
+		response.status_code=405
+		return(response)
 
 class AddAct(Resource):
 	def post(self):
@@ -153,24 +187,25 @@ class AddAct(Resource):
 		users = db.users
 		cat = db.categories
 
+		# print(posts.find_one({"actId":request_json['actId']}), users.find_one({"username":request_json['username']}),not('upvotes' in request_json.keys()),cat.find_one({"category":request_json['categoryName']}),re.match(timestamp_validate, request_json['timestamp']))
+		# print(request_json['timestamp'])
 		response=Response()
 		if(posts.find_one({"actId":request_json['actId']})==None and users.find_one({"username":request_json['username']})!=None and not('upvotes' in request_json.keys()) and cat.find_one({"category":request_json['categoryName']})!=None and re.match(timestamp_validate, request_json['timestamp'])!=None):
 			try:
 				img = base64.b64decode(request_json['imgB64'])
 				input_to_mongo=request_json
-				file=open(str(input_to_mongo['actId'])+'.txt','w')
+				file=open('/Users/malaika/Desktop/cloud_computing_project/img_b64/'+str(input_to_mongo['actId'])+'.txt','w')
 				file.write(request_json['imgB64'])
-				input_to_mongo['imgB64']='/home/ubuntu/flask/cloud_computing_project/img_b64'+str(input_to_mongo['actId'])+'.txt'
-
+				input_to_mongo['imgB64']='/Users/malaika/Desktop/cloud_computing_project/img_b64/'+str(input_to_mongo['actId'])+'.txt'
+				input_to_mongo['upvotes']=0
 				posts.insert_one(input_to_mongo)
 				cat_post =  cat.find_one({"category": request_json['categoryName']})
-				print(cat_post)
-				print(cat_post['count'])
 				update_cat = cat.update(cat_post,{"category": request_json['categoryName'],"count":cat_post['count']+1})
 				response.status_code=201
 				return(response)
 			
 			except Exception as e:
+				# print('exception: ',e)
 				response.status_code=400
 				return(response)
 			
@@ -214,15 +249,29 @@ class DelAct(Resource):
 		response.status_code=405	
 		return(response)
 
-class ListCategory(Resource):
-	print('ListCategory')
-	def get(self,category,startRange=None,endRange=None):
-		print('hello please')
-		#print(category)
+class getAct(Resource):
+	def post(self):
+		request_json = request.get_json()
+		actId=int(request_json['actId'])
+		username=request_json['username']
 		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
 		db = client.sla
-		
-		
+		posts = db.posts
+
+		r = posts.find_one({"actId":actId,"username":username})
+		if(r!=None):
+			response=Response()
+			response.status_code=200
+			return(response)
+		else:
+			response=Response()
+			response.status_code=400
+			return(response)
+
+class ListCategory(Resource):
+	def get(self,category,startRange=None,endRange=None):
+		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+		db = client.sla
 		col = db.posts
 		startRange=request.args.get('start')
 		endRange=request.args.get('end')
@@ -247,18 +296,15 @@ class ListCategory(Resource):
 					temp={}
 					for i in range(len(records)):
 						all_k=set(records[i].keys())
-						print(all_k)
 						all_k=all_k-keys
 						for j in all_k:
 							del records[i][j]
 						temp[records[i]['_id']]=records[i]
-					print('yaake')
+
 					list1=list(temp.keys())
 					list1.sort(reverse=True)
 					list1 = list1[startRange-1:endRange]
-					print(list1)
 					final_list = [temp[i] for i in list1]
-					print(final_list[0])
 					for i in range(len(final_list)):
 						del final_list[i]['_id']
 
@@ -268,18 +314,12 @@ class ListCategory(Resource):
 					response=jsonify(final_list)
 					response.status_code=200
 					return response
-		
 
 			else:
 				response= Response()
 				response.status_code=204
 				return(response)
 		else:
-		
-
-
-			
-
 			#print(records)
 			if(records.count()!=0):
 				if(records.count()>100):
@@ -287,18 +327,15 @@ class ListCategory(Resource):
 				else:
 					records=list(records)
 					keys={'actId','username','timestamp','caption','upvotes','imgB64'}
-					print(records)
 					for i in range(len(records)):
 						all_k=set(records[i].keys())
 						all_k=all_k-keys
 						for j in all_k:
 							del records[i][j]
-						print(records[i])
 						records[i]['imgB64']=open(records[i]['imgB64']).read()
 					response=jsonify(records)
 					response.status_code=200
 					return response
-
 
 			response=Response()
 			response.status_code=204
@@ -318,18 +355,78 @@ class ListCategory(Resource):
 		response.status_code=405
 		return response
 
+class GetFeed(Resource):
+	def get(self):
+		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+		db = client.sla
+		col = db.posts
 
+		posts = list(col.find().limit(10))
+		response_list=[]
+		for i in posts:
+			response_dict=i
+			response_dict.pop('_id',None)
+			url_b64=response_dict['imgB64']
+			with open(url_b64,'r') as img:
+				response_dict['imgB64']=img.read()
+			response_list.append(response_dict)
+		response=jsonify(response_list)
+		response.status_code=200
+		return(response)
+
+class GetUserFeed(Resource):
+	def post(self,username):
+		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+		db = client.sla
+		col = db.posts
+
+		posts = col.find({"username":username})
+		if(posts.count()==0):
+			response=Response()
+			response.status_code=204
+			return(response)
+
+		posts = list(posts)
+		response_list=[]
+		for i in posts:
+			response_dict=i
+			response_dict.pop('_id',None)
+			url_b64=response_dict['imgB64']
+			with open(url_b64,'r') as img:
+				response_dict['imgB64']=img.read()
+			response_list.append(response_dict)
+		response=jsonify(response_list)
+		response.status_code=200
+		return(response)
+
+class UpdateAct(Resource):
+	def post(self,actId):
+		client = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+		db = client.sla
+		col = db.posts
+
+		post = col.find_one_and_update({"actId":int(actId)} , {'$inc':{'upvotes':1}},return_document=ReturnDocument.AFTER)
+		response=Response()
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.status_code=200
+		return(response)
+
+
+api.add_resource(GetFeed,'/api/v1/categories/feed/acts')
 api.add_resource(ListCategory,'/api/v1/categories/<category>/acts')
 api.add_resource(AddUser, '/api/v1/users')
 api.add_resource(DelUser, '/api/v1/users/<username>')
+api.add_resource(UpdateAct,'/api/v1/acts/update_act/<actId>')
 api.add_resource(AddCategory, '/api/v1/categories')
 api.add_resource(DelCategory, '/api/v1/categories/<category>')
 api.add_resource(AddAct,'/api/v1/acts')
 api.add_resource(DelAct, '/api/v1/acts/<actId>')
 api.add_resource(ListNumActsForCat,'/api/v1/categories/<category>/acts/size')
-#api.add_resource(ListActsInRange,'/api/v1/categories/<categoryName>/acts?start=<startRange>&end=<endRange>')
+api.add_resource(Authenticate,'/api/v1/users/authenticate')
+api.add_resource(getAct,'/api/v1/users/acts/check_act')
+api.add_resource(GetUserFeed,'/api/v1/users/acts/<username>')
 
 
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(host='127.0.0.1',port=2000,debug = True)
