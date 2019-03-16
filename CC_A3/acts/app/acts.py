f3d4c110b06401c701c0e8e27643981d863bcedc
NinaadRao <ninaadrrao@gmail.com>
2019-03-16 20:16:28 +0530

    containers talking to each other

--- CC_A3/acts/app/acts.py
+++ CC_A3/acts/app/acts.py
@@ -1,3 +1,4 @@
+
 from flask import Flask,request,jsonify,make_response,Response
 from flask_restful import Resource, Api
 from pymongo import MongoClient,ReturnDocument
@@ -8,12 +9,12 @@ from flask_restful import reqparse
 import os
 import copy
 import requests
-
+import json
 pwd_validate = re.compile(r'\b[0-9a-f]{40}\b')
 timestamp_validate = re.compile(r'\b((0[1-9]|[1-2][0-9]|30)-(0[469]|11)-([1-2][0-9][0-9][0-9])|(0[1-9]|[1-2][0-9]|3[0-1])-(0[13578]|1[02])-([1-2][0-9][0-9][0-9])|((0[1-9]|[1-2][0-8])-(02)-([1-2][0-9][0-9][0-9]))|((29)-(02)-([1-2][0-9]([02468][48]|[13579][260])))|((29)-(02)-(1200|1600|2000|2400|2800))):[0-5][0-9]-[0-5][0-9]-[0-1][0-9]|[2][0-4]\b')
 #timestamp_validate= re.compile(r'\b())
 path=''
-instance_ip='54.198.209.122'
+instance_ip='http://18.215.245.30'
 client=MongoClient("mongodb://mongo:27017/?gssapiServiceName=mongodb")
 
 app = Flask(__name__)
@@ -118,32 +119,31 @@ class DelCategory(Resource):
 class AddAct(Resource):
 	def post(self):
 		request_json = request.get_json()
-
+		print('entered here')
 		db = client.sla
 		posts = db.posts
-		r = requests.get(instance_ip+':8080/'+'api/v1/users')
+		url=instance_ip+':8080/api/v1/users'
+		print(url)
+		r = requests.get(url,allow_redirects=False)
+		print('finished')
 		r = json.loads(r.text)
-		
+		print(r)
 
 		cat = db.categories
-		file=open('/home/ubuntu/flask/cloud_computing_project/check.txt','w')
-		file.write(str(posts.find_one({"actId":request_json['actId']})))
-		file.write('\n')
-		file.write(str(users.find_one({"username":request_json['username']})))
-		file.write(str(not('upvotes' in request_json.keys())))
-		file.write(str(cat.find_one({"category":request_json['categoryName']})))
-		file.write(str(request_json['timestamp']))
-		file.write(str(re.match(timestamp_validate, request_json['timestamp'])))		
-		file.close()
 		response=Response()
+		print(posts.find_one({"actId":request_json['actId']}))
+		print(request_json['username'] in r)
+		print(not('upvotes' in request_json.keys()))
+		print(cat.find_one({"category":request_json['categoryName']})!=None)
+		print(re.match(timestamp_validate, request_json['timestamp'])!=None)
 		if(posts.find_one({"actId":request_json['actId']})==None and request_json['username'] in r and not('upvotes' in request_json.keys()) and cat.find_one({"category":request_json['categoryName']})!=None and re.match(timestamp_validate, request_json['timestamp'])!=None):
 			try:
 				img = base64.b64decode(request_json['imgB64'])
 				input_to_mongo=request_json
 				
-				file=open('/home/ubuntu/flask/cloud_computing_project/img_b64/'+str(input_to_mongo['actId'])+'.txt','w')
+				file=open(str(input_to_mongo['actId'])+'.txt','w')
 				file.write(request_json['imgB64'])
-				input_to_mongo['imgB64']='/home/ubuntu/flask/cloud_computing_project/img_b64/'+str(input_to_mongo['actId'])+'.txt'
+				input_to_mongo['imgB64']=str(input_to_mongo['actId'])+'.txt'
 				input_to_mongo['upvotes']=0
 				posts.insert_one(input_to_mongo)
 				cat_post =  cat.find_one({"category": request_json['categoryName']})
@@ -156,12 +156,10 @@ class AddAct(Resource):
 			
 			except Exception as e:
 				print('exception: ',e)
-				file=open('/home/ubuntu/flask/cloud_computing_project/check.txt','w')
-				file.write(e)
-				file.close()
+				
 				response.status_code=400
 				return(response)
-			
+		print('entered here directly')
 		response.status_code=400
 		return(response)
 	def get(self):
@@ -228,7 +226,7 @@ class ListCategory(Resource):
 		record = col.find({"categoryName":category})
 		records=copy.deepcopy(record)
 		if(startRange!=None and endRange!=None):
-			file=open("/home/ubuntu/flask/a.txt","w")
+			file=open("a.txt","w")
 			file.write(str(records.count()))
 			file.close()
 			
